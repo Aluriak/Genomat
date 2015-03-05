c1293b5235ac52dd62355da68878a4ac8db7a573
Aluriak <lucas.bourneuf@laposte.net>
2015-03-05 14:19:10 +0100

    options for specify used initial phenotype

--- genomat/__main__.py
+++ genomat/__main__.py
@@ -12,8 +12,9 @@ Options:
     -v --version            show version
     -h --help               show this docstring                         
     --pop_size=<COUNT>      size of populations                         
-    --generations=<COUNT>[(,<COUNT>)]... number of generations computed 
+    --generations=<COUNT>[(,<COUNT>)]...    number of generations computed 
     --gene_number=<COUNT>   number of gene in a network                 
+    --initial_phenotype=<COUNT>[(,<COUNT>)]...  phenotype used as first phenotype 
     --mutation_rate=<RATE>  rate of mutation (float in [0;1])           
     --parent_number=<COUNT> number of parents for give a new individual 
     --save_config           save config as                              
@@ -35,6 +36,7 @@ Options:
 from collections        import ChainMap
 from genomat.population import Population
 from docopt             import docopt
+from numpy              import matrix
 import genomat.config as config 
 import genomat.stats  as stats
 import csv
@@ -68,6 +70,8 @@ if __name__ is '__main__':
             config_args[key] = float(val)
         elif key == config.GENERATION_COUNTS:
             config_args[key] = [int(_) for _ in val.split(',')]
+        elif key == config.INITIAL_PHENOTYPE:
+            config_args[key] = matrix(val.replace(',', ';'))
         else:
             config_args[key] = int(val)
     # load configuration from file
@@ -94,6 +98,7 @@ if __name__ is '__main__':
     # or use a randomly created phenotype
     phenotype = config.random_phenotype(size=configuration[config.GENE_NUMBER])
     configuration[config.INITIAL_PHENOTYPE] = phenotype
+    pop = None
 
     # for each run of generation, create pop, step, KO, count
     for generation_count in configuration[config.GENERATION_COUNTS]:
@@ -108,7 +113,8 @@ if __name__ is '__main__':
     
 
     # finalize all
-    stats.finalize()
+    if configuration[config.DO_STATS]:
+        stats.finalize(pop)
 
 
 
